7d4c2a59e98d73d31244745a4e49ce22608b6de3
Tejaswini <csintme@gmail.com>
2024-04-15 23:52:16 +0530

    folder structure

---
+++ task-manager/src/app/add-edit-task/add-edit-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-task.component.html',
+  styleUrl: './add-edit-task.component.scss'
+})
+export class AddEditTaskComponent {
+
+}
---
+++ task-manager/src/app/task-table/task-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-table.component.html',
+  styleUrl: './task-table.component.scss'
+})
+export class TaskTableComponent {
+
+}
